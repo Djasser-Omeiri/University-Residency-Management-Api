166a599d0054da126522a8c9d6f2310e10b54669
Djasser <djasser_omeiri.fs@univ-boumerdes.dz>
2026-03-06 18:09:45 +0100

    Setup solution and projects with 3-tier structure and database tables

---
+++ UniversityResidencyApi/Controllers/MealTypeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UniversityResidencyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MealTypeController : ControllerBase
+    {
+    }
+}
