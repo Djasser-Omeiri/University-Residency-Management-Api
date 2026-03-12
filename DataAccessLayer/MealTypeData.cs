9052db5e2848467e466388df3fa6a3430af92137
Djasser <djasser_omeiri.fs@univ-boumerdes.dz>
2026-03-13 00:24:48 +0100

    Implement Meal Type DAL

--- DataAccessLayer/MealTypeData.cs
+++ DataAccessLayer/MealTypeData.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,5 +24,106 @@ namespace DataAccessLayer
     }
     public class MealTypeData
     {
+        public static int AddMealType(MealAccessLogDTO mealAccessLogDto)
+        {
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            using (SqlCommand command = new SqlCommand("SP_AddMealType", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Name", mealAccessLogDto.MealTypeID);
+                command.Parameters.AddWithValue("@StartTime", mealAccessLogDto.AccessTime);
+                command.Parameters.AddWithValue("@EndTime", mealAccessLogDto.AccessTime);
+                var ReturnedValue = new SqlParameter("@ReturnVal", SqlDbType.Int)
+                {
+                    Direction = ParameterDirection.Output
+                };
+                command.Parameters.Add(ReturnedValue);
+                connection.Open();
+                command.ExecuteNonQuery();
+                return (int)ReturnedValue.Value;
+            }
+        }
+
+        public static bool DeleteMealType(int MealTypeID)
+        {
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            using (SqlCommand command = new SqlCommand("SP_DeleteMealType", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@MealTypeID", MealTypeID);
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                return rowsAffected == 1;
+            }
+        }
+
+        public static List<MealTypeDTO> GetAllMealTypes()
+        {
+            List<MealTypeDTO> MealTypesList = new List<MealTypeDTO>();
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SP_GetAllMealTypes", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MealTypesList.Add(new MealTypeDTO(
+                                reader.GetInt32(reader.GetOrdinal("MealTypeID")),
+                                reader.GetString(reader.GetOrdinal("Name")),
+                                reader.GetDateTime(reader.GetOrdinal("StartTime")),
+                                reader.GetDateTime(reader.GetOrdinal("EndTime"))
+                            ));
+                        }
+                    }
+                }
+                return MealTypesList;
+            }
+        }
+
+        public static MealTypeDTO GetMealTypeByID(int MealTypeID)
+        {
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            using (SqlCommand command = new SqlCommand("SP_GetMealTypeByID", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@MealTypeID", MealTypeID);
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new MealTypeDTO(
+                            reader.GetInt32(reader.GetOrdinal("MealTypeID")),
+                            reader.GetString(reader.GetOrdinal("Name")),
+                            reader.GetDateTime(reader.GetOrdinal("StartTime")),
+                            reader.GetDateTime(reader.GetOrdinal("EndTime"))
+                        );
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+        }
+
+        public static bool UpdateMealType(MealTypeDTO mealTypeDto)
+        {
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            using (SqlCommand command = new SqlCommand("SP_UpdateMealType", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@MealTypeID", mealTypeDto.MealTypeID);
+                command.Parameters.AddWithValue("@Name", mealTypeDto.Name);
+                command.Parameters.AddWithValue("@StartTime", mealTypeDto.StartTime);
+                command.Parameters.AddWithValue("@EndTime", mealTypeDto.EndTime);
+                connection.Open();
+                int AffectedRows = command.ExecuteNonQuery();
+                return AffectedRows > 0;
+            }
+        }
     }
 }
