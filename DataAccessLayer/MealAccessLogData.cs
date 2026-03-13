b797f5adb2705d074e44d5cdefcd87ec98a9b713
Djasser <djasser_omeiri.fs@univ-boumerdes.dz>
2026-03-13 17:11:44 +0100

    Implement Meal Access Log DAL

--- DataAccessLayer/MealAccessLogData.cs
+++ DataAccessLayer/MealAccessLogData.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,5 +24,49 @@ namespace DataAccessLayer
     }
     public class MealAccessLogData
     {
+        public static int AddMealAccessLog(MealAccessLogDTO mealAccessLogDto)
+        {
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            using (SqlCommand command = new SqlCommand("SP_AddMealAccess", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@CardID", mealAccessLogDto.CardID);
+                command.Parameters.AddWithValue("@MealTypeID", mealAccessLogDto.MealTypeID);
+                command.Parameters.AddWithValue("@AccessTime", mealAccessLogDto.AccessTime);
+                var ReturnedValue = new SqlParameter("@ReturnVal", SqlDbType.Int)
+                {
+                    Direction = ParameterDirection.Output
+                };
+                command.Parameters.Add(ReturnedValue);
+                connection.Open();
+                command.ExecuteNonQuery();
+                return (int)ReturnedValue.Value;
+            }
+        }
+        public static List<MealAccessLogDTO> GetAllMealAccessLogs()
+        {
+            List<MealAccessLogDTO> MealAccessLogsList = new List<MealAccessLogDTO>();
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SP_GetAllMealAccessLogs", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MealAccessLogsList.Add(new MealAccessLogDTO(
+                                reader.GetInt32(reader.GetOrdinal("AccessID")),
+                                reader.GetInt32(reader.GetOrdinal("CardID")),
+                                reader.GetInt32(reader.GetOrdinal("MealTypeID")),
+                                reader.GetDateTime(reader.GetOrdinal("AccessTime"))
+                            ));
+                        }
+                    }
+                }
+                return MealAccessLogsList;
+            }
+        }
     }
 }
